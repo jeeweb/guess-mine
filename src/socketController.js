1c623fe8417cd18b70c6af6266c34f0a7a610e11
jeeweb <52396673+jeeweb@users.noreply.github.com>
2022-12-18 12:17:19 +0900

    player 목록 추가 및 문제 단어 및 제출자(leader) 선정

--- src/socketController.js
+++ src/socketController.js
@@ -1,18 +1,30 @@
 import events from "./events.js";
+import { chooseWord } from "./words.js";
 
 let sockets = [];
+let inProgress = false;
+let word = null;
+
+const chooseLeader = () => sockets[Math.floor(Math.random() * sockets.length)];
 
 const socketController = (socket, io) => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
   const superBroadcast = (event, data) => io.emit(event, data);
   const sendPlayerUpdate = () => superBroadcast(events.playerUpdate, { sockets });
-
+  const startGame = () => {
+    if(inProgress === false) {
+      inProgress = true;
+      const leader = chooseLeader();
+      word = chooseWord();
+    }
+  }
   socket.on(events.setNickname, ({nickname}) => {
     socket.nickname = nickname;
     sockets.push({ id: socket.id, points: 0, nickname: nickname })
     broadcast(events.newUser, { nickname });
     //console.log(nickname);
     sendPlayerUpdate();
+    startGame();
   });
   socket.on(events.disconnect, () => {
     sockets = sockets.filter(aSocket => aSocket.id !== socket.id);
